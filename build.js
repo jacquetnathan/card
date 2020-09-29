73a6b198c4d338e8ff208cb18c9a5c3cb4fadf16
Jacquet Nathan <jacquet.nathan2@gmail.com>
2020-09-29 11:45:17 +0200

    add

---
+++ build.js
@@ -0,0 +1,54 @@
+'use strict'
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('               Jacquet Nathan'),
+  handle: chalk.white('jacquetnathan'),
+  work: chalk.white('Junior Web Developer'),
+  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~jacquetnathan'),
+  github: chalk.gray('https://github.com/') + chalk.green('jacquetnathan'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('nathan-jacquet'),
+  npx: chalk.red('npx') + ' ' + chalk.white('@jacquetnathan/card  (via GitHub Package Registry)'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelOpenSource: chalk.white.bold('Open Source:'),
+  labelnpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelCard: chalk.white.bold('       Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} / ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
+const npming = `${data.labelnpm}  ${data.npm}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+               npming + newline + // data.labelnpm + data.npm
+               githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               carding // data.labelCard + data.npx
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
